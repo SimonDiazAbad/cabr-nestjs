d279c65a0b2a4a9416cfa77e44ed31070e754261
Simon Diaz <simon.diaz.abad@gmail.com>
2023-09-09 19:24:01 -0400

    config and db module setup

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/cabr')],
+})
 export class DatabaseModule {}
