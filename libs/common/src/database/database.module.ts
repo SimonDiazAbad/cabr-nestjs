992b16d4672cc7f9fa27509642d32eef2f16269a
Simon Diaz <simon.diaz.abad@gmail.com>
2023-09-09 19:54:41 -0400

    db config completed

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+// import { ConfigModule } from '@app/common';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/cabr')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class DatabaseModule {}
