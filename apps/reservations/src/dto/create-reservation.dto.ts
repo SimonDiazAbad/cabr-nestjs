901190bad594fd185a276c991314db5a4c9373a2
Simon Diaz <simon.diaz.abad@gmail.com>
2023-09-09 22:00:38 -0400

    added validation

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,16 @@
+import { IsDate, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateReservationDto {
+  @IsDate()
   startDate: Date;
+  @IsDate()
   endDate: Date;
+
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
