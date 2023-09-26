5d528c4cf607de8de1cb5508e52eac4d736689a0
Simon Diaz <simon.diaz.abad@gmail.com>
2023-09-26 16:12:54 -0400

    added tcp ping logic

--- e2e/specs/healthcheck.app.e2e.spec.ts
+++ e2e/specs/healthcheck.app.e2e.spec.ts
@@ -1,3 +1,5 @@
+import { ping } from 'tcp-ping';
+
 describe('Health', () => {
   it('Reservations', async () => {
     const response = await fetch('http://reservations:3000');
@@ -7,5 +9,23 @@ describe('Health', () => {
   it('Auth', async () => {
     const response = await fetch('http://auth:3001');
     expect(response.ok).toBeTruthy();
+  });
+
+  it('Payments', async (done) => {
+    ping(
+      {
+        address: 'payments',
+        port: 3003,
+      },
+      (error, response) => {
+        if (error) {
+          fail(error);
+        } else {
+          console.log(response);
+          expect(response).toBeTruthy();
+          done();
+        }
+      },
+    );
   });
 });
