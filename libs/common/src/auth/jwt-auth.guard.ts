5f032e1fa1d38f54d60e751598db0beb38c53be7
Simon Diaz <simon.diaz.abad@gmail.com>
2023-09-27 12:12:24 -0400

    implemented roles

--- libs/common/src/auth/jwt-auth.guard.ts
+++ libs/common/src/auth/jwt-auth.guard.ts
@@ -3,15 +3,22 @@ import {
   CanActivate,
   ExecutionContext,
   Inject,
+  UnauthorizedException,
+  Logger,
 } from '@nestjs/common';
-import { Observable, map, tap } from 'rxjs';
+import { Observable, catchError, map, of, tap } from 'rxjs';
 import { AUTH_SERVICE } from '@app/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { UserDto } from '@app/common';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(@Inject(AUTH_SERVICE) private readonly authClient: ClientProxy) {}
+  private readonly logger = new Logger(JwtAuthGuard.name);
+  constructor(
+    @Inject(AUTH_SERVICE) private readonly authClient: ClientProxy,
+    private readonly reflector: Reflector,
+  ) {}
 
   canActivate(
     context: ExecutionContext,
@@ -24,15 +31,28 @@ export class JwtAuthGuard implements CanActivate {
       return false;
     }
 
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+
     return this.authClient
       .send<UserDto>('authenticate', {
         Authentication: jwt,
       })
       .pipe(
         tap((res) => {
+          if (roles) {
+            for (const role of roles) {
+              if (!res.roles?.includes(role)) {
+                throw new UnauthorizedException();
+              }
+            }
+          }
           context.switchToHttp().getRequest().user = res;
         }),
         map(() => true),
+        catchError((err) => {
+          this.logger.error(err);
+          return of(false);
+        }),
       );
   }
 }
